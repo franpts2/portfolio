7287a39c7f19179c26065e1e5507a3d3f450d53a
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-10-04 23:02:23 +0100

    feat(home): Added a more color consistent image for dark mode

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../components/ThemeProvider.tsx";
 
 const HomePage = () => {
 	return (
 		<div className="relative min-h-screen flex items-center">
 			<div className="mx-auto text-center max-w-3xl relative">
-				<img
-					src="/sand-colored-me.png"
-					alt="Francisca Portugal"
-					className="pointer-events-none select-none absolute"
-					style={{
-						width: 600,
-						right: -290,
-						bottom: -65,
-						transform: "rotate(-15deg)",
-					}}
-				/>
-                <h1 className="text-7xl font-black font-family-heading text-primary-accent relative inline-block pointer-events-none select-none">
+				{(() => {
+					const { isDark } = useContext(ThemeContext);
+					const src = isDark ? "/night-colored-me.png" : "/sand-colored-me.png";
+					return (
+						<img
+							src={src}
+							alt="Francisca Portugal"
+							className="pointer-events-none select-none absolute"
+							style={{
+								width: 600,
+								right: -290,
+								bottom: -65,
+								transform: "rotate(-15deg)",
+							}}
+						/>
+					);
+				})()}
+				<h1 className="text-7xl font-black font-family-heading text-primary-accent relative inline-block pointer-events-none select-none">
 					Francisca Portugal
 				</h1>
 
