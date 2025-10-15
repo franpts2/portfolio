2a85a40e31e65609c19f2c17ce06bbf056739daa
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-10-15 15:47:45 +0100

    fix(portfolio): Added an animation to the theme switcher

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -9,13 +9,23 @@ const ThemeSwitcher = () => {
 
 	return (
 		<div
-			className="cursor-pointer mx-1 text-primary"
+			className="cursor-pointer mx-1 text-primary transition-transform duration-300 ease-in-out"
 			onClick={toggleTheme}
 		>
 			{isDark ? (
-				<Icon icon={icons.light.outline} height={24} />
+				<Icon
+					icon={icons.light.outline}
+					height={24}
+					className="transition-transform duration-300 ease-in-out"
+					style={{ transform: "rotate(30deg)" }}
+				/>
 			) : (
-				<Icon icon={icons.dark.outline} height={24} />
+				<Icon
+					icon={icons.dark.outline}
+					height={24}
+					className="transition-transform duration-300 ease-in-out"
+					style={{ transform: "rotate(0deg)" }}
+				/>
 			)}
 		</div>
 	);
