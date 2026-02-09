b3e1cad8b9e2a232c7926a40c65369c7a201cfcd
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-02-09 21:50:29 +0000

    feat(projects): Added icons for languages and technologies

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Badge from "./Badge.tsx";
 import { Icon } from "@iconify/react";
 import { icons } from "../assets/icons.js";
+import { ThemeContext } from "./ThemeProvider.tsx";
 
 type CardProps = {
 	title: string;
 	description: string;
+	tech: string[];
 	isDone: boolean;
 	isCollab: boolean;
 };
@@ -13,9 +15,12 @@ type CardProps = {
 const Card: React.FC<CardProps> = ({
 	title,
 	description,
+	tech,
 	isDone,
 	isCollab,
 }) => {
+	const { isDark } = useContext(ThemeContext);
+	const iconTheme = isDark ? "dark" : "light";
 	return (
 		<div className="bg-secondary-bg rounded-2xl p-8 flex flex-col w-xs gap-3 cursor-pointer hover:scale-105 shadow-lg">
 			{/* header */}
@@ -29,7 +34,17 @@ const Card: React.FC<CardProps> = ({
 
 			{/* icons */}
 			<div className="flex flex-row justify-between items-center mt-auto">
-				<Icon icon={icons.code.fill} height={24} />
+				<div className="flex flex-row gap-1">
+					{tech.map((t, index) => (
+						<img
+							key={index}
+							src={`https://skillicons.dev/icons?i=${t}&theme=${iconTheme}`}
+							alt={t}
+							width="30"
+							height="30"
+						/>
+					))}
+				</div>
 				<Icon
 					icon={isCollab ? icons.group.fill : icons.about.fill}
 					height={24}
