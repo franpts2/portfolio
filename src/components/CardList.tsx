d36c9a0dd1512a59890a55d2e2f2ec2b6232d2d1
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-02-09 18:33:19 +0000

    fix(projects): fixed layout of projects page

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -2,42 +2,49 @@ import React from "react";
 import Card from "./Card.tsx";
 
 const projects = [
-	{ title: "focusly", description: "All-in-one study app", isDone: true, isCollab: true },
+	{
+		title: "focusly",
+		description: "All-in-one study app",
+		isDone: true,
+		isCollab: true,
+	},
 	{
 		title: "junto.",
-		description:
-			"Social platform for sharing music, films, and books",
-        isDone: true,
+		description: "Social platform for sharing music, films, and books",
+		isDone: true,
 		isCollab: true,
 	},
 	{
 		title: "artflow",
 		description: "Freelancing website dedicated to artists ",
-        isDone: true,
+		isDone: true,
 		isCollab: true,
 	},
 	{
 		title: "portfolio",
-		description:
-			"This personal portfolio website!",
-        isDone: false,
+		description: "This personal portfolio website!",
+		isDone: false,
 		isCollab: false,
 	},
 ];
 
 const CardList = () => {
-	return (
-		<div className="flex flex-row gap-4">
-			{projects.map((project) => (
-				<Card
-					title={project.title}
-					description={project.description}
-                    isDone={project.isDone}
-					isCollab={project.isCollab}
-				/>
-			))}
-		</div>
-	);
+    return (
+        <div className="flex flex-col gap-8 items-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                {projects.map((project, index) => (
+                    <Card
+                        key={index}
+                        title={project.title}
+                        description={project.description}
+                        isDone={project.isDone}
+                        isCollab={project.isCollab}
+                    />
+                ))}
+            </div>
+            <p className="text-secondary mb-4">& many more to come...</p>
+        </div>
+    );
 };
 
 export default CardList;
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import Badge from "../components/Badge.tsx";
-import Card from "../components/Card.tsx";
 import CardList from "../components/CardList.tsx";
 
 const ProjectsPage = () => {
-	return (
-		<div className="relative min-h-screen flex items-center justify-center">
-			<CardList />
-		</div>
-	);
+    return (
+        <div className="relative min-h-screen flex flex-col items-center gap-10">
+            <h1 className="font-black text-4xl text-primary-accent mt-10">Projects</h1>
+            <CardList />
+        </div>
+    );
 };
 
 export default ProjectsPage;
