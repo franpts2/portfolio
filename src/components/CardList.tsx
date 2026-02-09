0140016b9041eca7a3c188986b03f9b962e4b31e
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-02-09 20:43:47 +0000

    chore(projects): Hardcoded (for now) personal projects data

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -26,6 +26,66 @@ const projects = [
 		isDone: false,
 		isCollab: false,
 	},
+    {
+		title: "tix",
+		description: "Social media platform for concert tracking and stats",
+		isDone: false,
+		isCollab: false,
+	},
+    {
+		title: "space wars",
+		description: "A simple Space Invaders Java game using Lanterna.",
+		isDone: true,
+		isCollab: true,
+	},
+    {
+		title: "fernando oceano's quest",
+		description: "2D adventure game with time-tracking, collectibles and sound.",
+		isDone: true,
+		isCollab: false,
+	},
+    {
+		title: "whack-a-diglett",
+		description: "Whack-a-Mole inspired game in Minix ",
+		isDone: true,
+		isCollab: true,
+	},
+    {
+		title: "'24 olympics db",
+		description: "2024 Olympics Database",
+		isDone: true,
+		isCollab: true,
+	},
+    {
+		title: "route planner",
+		description: "Implementation of several route-planning algorithms",
+		isDone: true,
+		isCollab: true,
+	},
+    {
+		title: "knaptruck",
+		description: "Solver for a packing/knapsack-style problem using several algorithmic approaches",
+		isDone: true,
+		isCollab: true,
+	},
+    {
+		title: "seriallink",
+		description: "Implementation of a serial-port-based file transfer protocol. ",
+		isDone: true,
+		isCollab: true,
+	}, 
+    {
+		title: "netfetch",
+		description: "Lab project implementing a file-download client in C",
+		isDone: true,
+		isCollab: true,
+	}, 
+    {
+		title: "svgtopng",
+		description: "Parser of simple SVG files, rendering them to PNG images",
+		isDone: true,
+		isCollab: true,
+	},
 ];
 
 const CardList = () => {
