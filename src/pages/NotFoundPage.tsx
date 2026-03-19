17a8ac6d2f4995e12b7c134d7f76a7c5ef135940
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-03-19 19:10:15 +0000

    fix(projects): made non existent projects redirect to 404

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const NotFoundPage = () => {
-  return (
-    <div>404</div>
-  )
-}
+	return <div>404</div>;
+};
 
-export default NotFoundPage
+export default NotFoundPage;
